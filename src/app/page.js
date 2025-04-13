3211844c8b0013cb1215937471e6d0f15d94e7c0
DandBDevloper <dubaidevlopers@gmail.com>
2025-04-14 00:21:39 +0400

    intial error fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import Home_V1 from "./(home)/home-v1/page";
+// import Home_V1 from "./(home)/home-v1/page";
+import Home_V2 from "./(home)/home-v2/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
@@ -8,7 +9,7 @@ export const metadata = {
 export default function MainRoot() {
   return (
     <Wrapper>
-      <Home_V1 />
+      <Home_V2 />
     </Wrapper>
   );
 }
