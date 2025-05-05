978497e55ecf9350ce893927178ddd14193180d1
Ameer Mohammed <ameer.mohammed@DB-LTP-1113-Ameer.local>
2025-05-05 11:16:09 +0400

    updated next config for images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,40 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['backend.thetopmasters.com', 'anotherdomain.com','enquiries.estate', '127.0.0.1', 'd3h330vgpwpjr8.cloudfront.net'], // Allowed domains for next/image
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',            // must match your <Image> URL
+        hostname: 'backend.thetopmasters.com',
+        port: '',                     // default
+        pathname: '/**',              // allow all paths under /storage/…
+      },
+      {
+        protocol: 'https',
+        hostname: 'anotherdomain.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'enquiries.estate',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',             // if you ever serve from localhost over http
+        hostname: '127.0.0.1',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd3h330vgpwpjr8.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
